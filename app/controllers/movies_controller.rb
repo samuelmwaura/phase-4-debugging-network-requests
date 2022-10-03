759cfbfccfc52925b05fe2920d4d0b64fceb193f
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-03 17:10:02 +0300

    Completed the code along

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,23 @@
 class MoviesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_response
   
   def index
     movies = Movie.all
-    render json: movies
+    render json: movies, status: :created
   end
 
+  def create
+    bird = Bird.create!(movie_params)
+    render json: bird
+  end
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url,:category,:discount, :female_director)
+  end
+
+  def render_record_invalid_response(invalid)
+    render json: {errors:invalid.record.errors}, tatus: :unprocessable_entity 
+  end
 end
